cb028e199b6cb529ee3f22299a3262886108357c
forever <freeantwalk@163.com>
2019-11-23 16:41:38 +0800

    提交工程到本地仓库

---
+++ git_first/src/com/fxq/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.fxq.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+        System.out.println("this is my first project ");
+    }
+}
